e9a50c0b0513cf9a69f73d7b218ace08e9a20150
Ryousuke Hasimoto <hasimoto.r.uddhi@gmail.com>
2026-04-29 19:52:39 +0900

    fix init order

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 import os
 
 app = Flask(__name__)
-init_db()
+
 
 DB_FILE = "/tmp/recipe.db"
 
@@ -30,6 +30,8 @@ def init_db():
 
     conn.commit()
     conn.close()
+
+init_db()
 
 #食材追加
 def add_ingredient_db(item, qty):
@@ -290,7 +292,3 @@ if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    
-if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
